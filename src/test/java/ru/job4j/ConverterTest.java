aae6f46b124e2a7f2f69d5b23c73f2d23cd5a209
Diletant <al@naun.com>
2024-10-19 10:15:28 +0700

    Добавлены тесты для класса Converter

---
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float input = 140;
+        float expected = 2;
+        double output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+            assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert180RblThen3Dollar() {
+        float input = 180;
+        float expected = 3;
+        float output  = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert0RblThen0Dollar() {
+        float input = 0;
+        float expected = 0;
+        float output  = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
