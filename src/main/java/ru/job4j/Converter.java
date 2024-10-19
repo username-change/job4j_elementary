26d8d493e599c212b611997077d51c17a17dbea3
Diletant <al@naun.com>
2024-10-19 10:57:00 +0700

    удалены однострочные комментарии.

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -2,12 +2,10 @@ package ru.job4j;
 
 public class Converter {
     public static double rubleToEuro(double value) {
-        // Assuming the conversion rate is 1 Euro = 90 Rubles
         return value / 90;
     }
 
     public static double rubleToDollar(double value) {
-        // Assuming the conversion rate is 1 Dollar = 75 Rubles
         return value / 75;
     }
 }
