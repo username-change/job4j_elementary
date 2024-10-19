9d40124bc41ceb8dbbdce4f889727b9a2db0f619
Diletant <al@naun.com>
2024-10-19 15:47:56 +0700

    Тестирование Класса Расстояния между Точками [#505280]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+    @Test
+    void whenPoints00And20Then2() {
+        int x1 = 0, y1 = 0, x2 = 2, y2 = 0;
+        double expected = 2.0;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPointsMinus5Minus4And1And1Then2Dot83() {
+        int x1 = -5, y1 = -4, x2 = 1, y2 = 1;
+        double expected = 7.81;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPoints4And6And5And3ThenDot16() {
+        int x1 = 4, y1 = 6, x2 = 5, y2 = 3;
+        double expected = 3.16;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
