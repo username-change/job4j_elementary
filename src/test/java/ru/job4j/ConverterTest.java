c4506a3052ff919f81544c10d74a206524352b31
Diletant <al@naun.com>
2024-10-20 16:30:55 +0700

    испавлены синтаксические ошибки, мешающие компиляции проекта

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -6,11 +6,11 @@ import static org.assertj.core.api.Assertions.*;
 class ConverterTest {
 
     @Test
-    void whenConvert140RblThen2Euro() {
+    void whenConvert140RblThen2EuroThen1Dot86() {
         float input = 140;
-        float expected = 1.8666f;
+        float expected = 1.8666F;
         float output  = Converter.rubleToDollar(input);
-        float value = 0.0001f;
+        float value = 0.0001F;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
 
@@ -19,16 +19,16 @@ class ConverterTest {
         float input = 0;
         float expected = 0;
         float output  = Converter.rubleToEuro(input);
-        float value = 0.0001f;
+        float value = 0.0001F;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
 
     @Test
-    void whenConvert180RblThen3Dollar() {
+    void whenConvert180RblThen3DollarThen2Dot4() {
         float input = 180;
-        float expected = 2.4f;
+        float expected = 2.4F;
         float output = Converter.rubleToDollar(input);
-        float value = 0.0001f;
+        float value = 0.0001F;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
 }
