0c9b20fec1010dd4805d57ba18dc9dc32cd46a3e
Diletant <al@naun.com>
2024-10-20 11:59:38 +0700

    исправлены ошибки форматирования в классе Rectangle

--- src/main/java/ru/job4j/condition/Rectangle.java
+++ src/main/java/ru/job4j/condition/Rectangle.java
@@ -1,12 +1,12 @@
 package ru.job4j.condition;
 
 public class Rectangle {
-    public static double diagonal(double length, double width){
-        return Math.sqrt(Math.pow(length, 2) + Math.pow(width,2));
+    public static double diagonal(double length, double width) {
+        return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
     }
 
     public static void main(String[] args) {
-        double result = diagonal(3,4);
+        double result = diagonal(3, 4);
         System.out.println(result);
     }
 }
