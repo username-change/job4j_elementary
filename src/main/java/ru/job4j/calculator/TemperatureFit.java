07e6e4a9b5f97819f92bd64f7fb6c6699b6589be
Diletant <al@naun.com>
2024-10-20 18:19:50 +0700

    испавлены синтаксические ошибки

--- src/main/java/ru/job4j/calculator/TemperatureFit.java
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -1,19 +1,23 @@
-    package ru.job4j.calculator;
+package ru.job4j.calculator;
 
-    public class TemperatureFit {
-        public static double idealTemperatureForFruits(int temperature) {
-            return temperature - 2.0;
-        }
-
-        public static double idealTemperatureForMeat(int temperature) {
-            return temperature - 5.0;
-        }
-
-        public static void main(String[] args) {
-            int temperature = 10;
-            double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
-            double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
-            System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
-            System.out.println("Ideal temperature for meat is " + meatTemp + "°C");
-        }
+public class TemperatureFit {
+    public static double idealTemperatureForFruits(int temperature) {
+        return temperature - 2.0;
     }
+
+    public static double idealTemperatureForMeat(int temperature) {
+        return temperature - 5.0;
+
+    }
+
+    public static void main(String[] args) {
+        int temperature = 10;
+        double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
+        double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
+        System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
+        System.out.println("Ideal temperature for meat is " + meatTemp + "°C");
+
+    }
+
+}
+
