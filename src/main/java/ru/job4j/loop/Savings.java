28483d17509d8295a1c5046bd0492d973518abb9
Diletant <al@naun.com>
2024-10-23 10:51:17 +0700

    5.6. Простые числа [#505215 #518413]

--- src/main/java/ru/job4j/loop/Savings.java
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -1,4 +1,18 @@
 package ru.job4j.loop;
 
 public class Savings {
+    public static int years(double goal, double annualDeposit, double percent) {
+        int years = 0;
+        double total = 0;
+        do {
+            total += (total * (percent / 100));
+            total += annualDeposit;
+            years++;
+        } while (total < goal);
+        return years;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(years(500, 100, 5));
+    }
 }
