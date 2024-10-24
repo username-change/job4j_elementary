5a9814b27714897c0d8ac451ead262fec1779aaa
Diletant <al@naun.com>
2024-10-24 16:00:55 +0700

    Упорядочить массив [#505228 #518762]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -5,10 +5,25 @@ import java.util.Arrays;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-        Arrays.sort(array);
+        int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
+        array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
+        array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
 
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp1 = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp1;
+                    isSorted = false;
+                }
+            }
+
+            for (int index = 0; index < array.length; index++) {
+                System.out.println(array[index]);
+            }
         }
     }
 }
