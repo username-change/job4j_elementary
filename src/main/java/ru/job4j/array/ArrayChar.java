ae1eadc518bfb58fbd90a20135fb7ffe089f0888
Diletant <al@naun.com>
2024-10-25 09:31:07 +0700

    Слово начинается с ... [#505233 #518897]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,10 +3,6 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        if (prefix.length > word.length) {
-            return false;
-        }
-
         for (int i = 0; i < prefix.length; i++) {
             if (word[i] != prefix[i]) {
                 result = false;
