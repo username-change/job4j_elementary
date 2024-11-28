cdc318d5007c424a0739659da0190eb2a761e3ee
Diletant <al@naun.com>
2024-11-28 14:36:47 +0700

    5.1. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/cast/Plane.java
@@ -0,0 +1,9 @@
+package ru.job4j.cast;
+
+public class Plane implements Vehicle {
+	@Override
+	public void move() {
+		System.out.println("самолет летает по воздуху");
+	}
+	
+}
---
+++ src/main/java/ru/job4j/cast/VehicleMain.java
@@ -0,0 +1,14 @@
+package ru.job4j.cast;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle plane = new Plane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+        
+        Vehicle[] vehicles = new Vehicle[]{plane, train, bus};
+        for (Vehicle object : vehicles) {
+            object.move();
+        }
+    }
+}
